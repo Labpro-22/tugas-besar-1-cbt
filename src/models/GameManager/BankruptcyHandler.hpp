bc2c9009323e4db5d3cd1bddd6f52eb227ce1156
Valentino Daniel Kusumo <19624107@mahasiswa.itb.ac.id>
2026-04-18 19:45:27 +0700

    refactor: restore BankruptcyHandler.cpp

--- src/models/GameManager/BankruptcyHandler.hpp
+++ src/models/GameManager/BankruptcyHandler.hpp
@@ -7,22 +7,21 @@
 #include "LiquidationPanel.hpp"
 using namespace std;
 
-
-    class BankruptcyHandler {
-    private:
-        Player& debtor;
-        Player* creditor;
-        int debtAmount;
-        LiquidationPanel panel;
-    public:
-        BankruptcyHandler(Player& debtor, Player* creditor, int debt);
-        int calculateMaxLiquidation();
-        bool canCoverDebt();
-        bool initiateLiquidation();
-        void sellPropertyToBank(Property& prop);
-        void mortgageProperty(Property& prop);
-        void declareBankrupt();
-        void transferAssets();
-        void repossessProperties();
-        void auctionRepossessedProperties();
+class BankruptcyHandler {
+private:
+    Player& debtor;
+    Player* creditor;
+    int debtAmount;
+    LiquidationPanel panel;
+public:
+    BankruptcyHandler(Player& debtor, Player* creditor, int debt);
+    int calculateMaxLiquidation();
+    bool canCoverDebt();
+    bool initiateLiquidation();
+    void sellPropertyToBank(Property& prop);
+    void mortgageProperty(Property& prop);
+    void declareBankrupt();
+    void transferAssets();
+    void repossessProperties();
+    void auctionRepossessedProperties();
 };
